25f826aef60b1ae836688fa47a4074e450560a3f
bamanguragain <159222194+bamanguragain@users.noreply.github.com>
2024-09-23 09:48:14 +0545

    Create leaderboard.js
    Leaderboard for Weave-Game Test1

---
+++ leaderboard.js
@@ -0,0 +1,20 @@
+<!-- index.ejs -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Leaderboard</title>
+</head>
+<body>
+  <h1>Leaderboard</h1>
+  <ul>
+    <% leaderboards.forEach((leaderboard) => { %>
+      <li><%= leaderboard.name %> - <%= leaderboard.score %></li>
+    <% }); %>
+  </ul>
+  <form action="/leaderboard" method="post">
+    <input type="text" name="name" placeholder="Name">
+    <input type="number" name="score" placeholder="Score">
+    <button type="submit">Submit</button>
+  </form>
+</body>
+</html>
